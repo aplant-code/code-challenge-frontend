444e6f8192e4b0c24dc05f33e70f2a25f0fbed56
Alex Plant <alex.c.plant@gmail.com>
2020-01-08 21:28:11 +1100

    Clean up hard coded strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { RacesService } from 'src/services/races.service';
 import { Race } from 'src/models/race';
 
+const HORSE_TYPE:string = 'horseracing';
+const GREYHOUND_TYPE:string = 'greyhoundracing';
+const HARNESS_TYPE:string = 'harnessracing';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +15,7 @@ export class AppComponent implements OnInit {
   title = 'code-challenge-frontend';
   private races: Race[] = [];
   private filteredRaces: Race[] = [];
+
 
   private displayedColumns: string[] = ['eventIcon', 'eventTitle', 'eventTime'];
 
@@ -29,13 +34,13 @@ export class AppComponent implements OnInit {
   private filterRaces(type: number) {
     switch (type) {
       case 1:
-        this.filteredRaces = this.races.filter(race => race.icon == "horseracing");
+        this.filteredRaces = this.races.filter(race => race.icon == HORSE_TYPE);
         break;
       case 2:
-        this.filteredRaces = this.races.filter(race => race.icon == "greyhoundracing");
+        this.filteredRaces = this.races.filter(race => race.icon == GREYHOUND_TYPE);
         break;
       case 3:
-        this.filteredRaces = this.races.filter(race => race.icon == "harnessracing");
+        this.filteredRaces = this.races.filter(race => race.icon == HARNESS_TYPE);
         break;
       default:
         this.filteredRaces = this.races;
